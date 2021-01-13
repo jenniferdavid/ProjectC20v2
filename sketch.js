6107b8a2d0f593a026815b36c6483e39156693e6
jenniferdavid <jenniferdavid87@gmail.com>
2021-01-13 18:46:39 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -36,7 +36,7 @@ function draw() {
     background(255);
     //Write condition here to evalute if tom and jerry collide
 
-    if((tomCat.x - jerryMouse.x)+70 < (tomCat.width - jerryMouse.width)/2){
+    if((tomCat.x - jerryMouse.x) < (tomCat.width - jerryMouse.width)/2 -166){
         tomCat.velocityX = 0;
         tomCat.addAnimation("catStop",catImg3);
         tomCat.changeAnimation("catStop");
